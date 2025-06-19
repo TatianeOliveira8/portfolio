4753d0a2ef163563d8a187cb631daefe76dd2bf9
Tatiane Oliveira <164065945+TatianeOliveira8@users.noreply.github.com>
2025-06-19 10:11:27 -0300

    remove unused variable location and import location

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Inicio from './pages/Inicio';
 import Curriculo from './pages/curriculo.jsx';
